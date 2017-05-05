7570c7bc1bcf588da4313a70d35d33a0938abd23
Tong Phuoc Hung <gcbtfh@gmail.com>
2017-05-05 11:51:02 +0700

    Just push

---
+++ app/src/main/java/demo/perfsoft/com/lambdaexpressiondemo/MainActivity.java
@@ -0,0 +1,21 @@
+package demo.perfsoft.com.lambdaexpressiondemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Hello Lambda expression", Toast.LENGTH_SHORT).show());
+    }
+}
